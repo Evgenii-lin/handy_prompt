0a7f392824c19b16cae08ab99ab4d6406f87c2b8
Evgenii <savenkovea@gmail.com>
2026-08-27 23:55:54 +0300

    Delete prompt_app directory

--- prompt_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Prompt
-
-
-@admin.register(Prompt)
-class PromptAdmin(admin.ModelAdmin):
-    list_display = ['name', 'created_at', 'updated_at']
-    search_fields = ['name', 'content']
-    ordering = ['name']
--- prompt_app/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class PromptAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'prompt_app'
-    verbose_name = 'Prompt Templates'
