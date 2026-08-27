8aabd057161edd65ca89ac3b847de430d4d429e4
Evgenii <savenkovea@gmail.com>
2026-08-27 23:55:42 +0300

    Delete prompt_website directory

--- prompt_website/wsgi.py
+++
@@ -1,7 +0,0 @@
-"""WSGI config for prompt_website project."""
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prompt_website.settings')
-
-application = get_wsgi_application()
